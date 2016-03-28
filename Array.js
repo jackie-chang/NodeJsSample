a1ee38ae278b169505c2cb7fb51a88a245a7be6e
jackie-chang <chjackiekimo@hotmail.com>
2016-03-29 01:40:36 +0800

    first

---
+++ Array.js
@@ -0,0 +1,28 @@
+var foo = [];
+
+foo.push(1);
+
+getArrayItems(foo);//1
+
+foo.push(2);
+
+getArrayItems(foo);//1 2
+
+foo.unshift(3);
+
+getArrayItems(foo);//3 1 2 
+
+foo.unshift(4);
+
+getArrayItems(foo); // 4 3 1 2
+
+
+
+function getArrayItems(arrayObject) {
+    var answer="";
+    arrayObject.forEach(function(element) {
+        answer+=' '+element;
+    }, this);
+    console.log(answer);
+};
+
