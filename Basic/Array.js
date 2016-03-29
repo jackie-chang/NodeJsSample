073b9f7c23ecabcfb01a48687ffb4279cbfb03d3
jackie-chang <chjackiekimo@hotmail.com>
2016-03-29 13:03:09 +0800

    modefied

--- Basic/Array.js
+++ Basic/Array.js
@@ -8,11 +8,7 @@ foo.push(2);
 
 getArrayItems(foo);//1 2
 
-<<<<<<< HEAD
 foo.unshift(3);
-=======
-foo.unshift(4);
->>>>>>> 6b511b34f6b486ceee29c3303d4b994a196bf0b1
 
 getArrayItems(foo);//3 1 2 
 
