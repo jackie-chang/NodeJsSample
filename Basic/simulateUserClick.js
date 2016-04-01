b474a080970c22795d44c56ad3f7a7879c895084
jackie-chang <chjackiekimo@hotmail.com>
2016-04-01 13:20:33 +0800

    add simulateUserClick

---
+++ Basic/simulateUserClick.js
@@ -0,0 +1,32 @@
+//first simulateUserClick Sample
+
+function longRunningOperation(callback) {
+    setTimeout(callback,3000);
+}
+
+function userClicked() {
+    console.log('starting a long operation');
+    longRunningOperation(function () {
+        console.log('ending a long operation');
+    });
+}
+
+userClicked();
+
+//first simulateUserClick Sample2
+
+function longRunningOperation2(callback) {
+    setTimeout(callback,3000);
+}
+
+function userClicked2(request) {
+    console.log('starting a long operation '+request.id);
+    console.log('');
+    longRunningOperation2(function () {
+        console.log('ending a long operation '+request.id);
+        console.log('');
+    })
+}
+
+userClicked2({id:'test1'});
+userClicked2({id:'test2'});
